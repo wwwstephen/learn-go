2d142337f2e2a7019f55a29cfd657aa4a065ee23
wwwstephen <ennisstephen@gmail.com>
2025-03-08 00:10:15 +0000

    did maps

---
+++ 07_maps/dictionary.go
@@ -0,0 +1,65 @@
+package main
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(key string) (string, error) {
+	value, ok := d[key]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return value, nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_, ok := d[key]
+
+	if ok {
+		return ErrWordExists
+	} else {
+		d[key] = value
+		return nil
+	}
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+
+	return nil
+}
